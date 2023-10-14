dcfe45a391e09e9bdd076264e0358084d264156a
Mark Rodgers <markrodgers.dev@gmail.com>
2023-10-14 20:53:31 +0100

    Create the grid container and populate it with cells

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("test")
+const gridContainer = document.querySelector("#grid-container");
+const gridContainerWidth = gridContainer.offsetWidth;
+const gridContainerHeight = gridContainer.offsetHeight;
+
+let sizeOfGrid = 4;
+
+//Create the required cells in the container and set their size accordingly
+for(let i = 0; i < (sizeOfGrid * sizeOfGrid); i++){
+    const cellDiv = document.createElement("div");
+    cellDiv.classList.add("cell");
+    cellDiv.style.width = `${gridContainerWidth/sizeOfGrid}px`;
+    cellDiv.style.height = `${gridContainerHeight/sizeOfGrid}px`;
+    gridContainer.appendChild(cellDiv);
+}
