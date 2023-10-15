626376108994e6656a47ed4fd2967c0ae3226b0e
Mark Rodgers <markrodgers.dev@gmail.com>
2023-10-15 22:16:52 +0100

    Move code functionality into functions and Add hover effect to paint cells

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const gridContainer = document.querySelector("#grid-container");
+function createGrid(gridSize){
+    const gridContainer = document.querySelector("#grid-container");
 
-let sizeOfGrid = 4;
-
-for(let i = 0; i < sizeOfGrid; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-
-    for(let j = 0; j < sizeOfGrid; j++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell);
+    for(let i = 0; i < gridSize; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+    
+        for(let j = 0; j < gridSize; j++){
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+    
+        gridContainer.appendChild(row);
     }
 
-    gridContainer.appendChild(row);
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", draw)
+    });
 }
+
+function draw(event){
+    event.target.style.backgroundColor = "black";  
+}
+
+let sizeOfGrid = 4;
+createGrid(sizeOfGrid)
