ba772ada007fcf58ce6c5460bbe5daecc3eb6497
Mark Rodgers <markrodgers.dev@gmail.com>
2023-10-16 17:30:39 +0100

    Change the mouse over coloring in to click and hold to color in

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 const gridContainer = document.querySelector("#grid-container");
 const gridSizeSlider = document.querySelector("#slider");
-const clearGrid = document.querySelector("#clear");
+const clearGridButton = document.querySelector("#clear");
 const defaultGridSize = 10;
+let isClicked = false;
 
 gridSizeSlider.addEventListener("change", resetGrid);
-clearGrid.addEventListener("click", resetGrid);
+clearGridButton.addEventListener("click", resetGrid);
+
+gridContainer.addEventListener("mousedown", (event) => {
+    //Prevents default dragable behaviour of mouse-down from occuring over already painted cells
+    event.preventDefault();
+    isClicked = true;
+
+    /*
+    Prevents an unwanted effect: Clicking on the outer border
+    would color the whole grid container 
+    */
+    if(event.target.id === "grid-container"){
+        return
+    }
+    /*
+    This call to draw was needed to colour in the target cell 
+    where the click occurs and subsequent draws will occur when 
+    the cursor enters a cell via mouse enter
+    */
+    draw(event);
+});
+
+/*
+Added the listener to the document instead of the gridContainer
+so that if the user keeps left click depressed on the grid and 
+moves their cursor off the grid and releases the boolean is updated
+*/
+document.addEventListener("mouseup", () => {
+    isClicked = false;
+});
 
 function createGrid(gridSize){
     gridContainer.textContent = "";
@@ -12,13 +42,13 @@ function createGrid(gridSize){
     for(let i = 0; i < gridSize; i++){
         const row = document.createElement("div");
         row.classList.add("row");
-    
+        
         for(let j = 0; j < gridSize; j++){
             const cell = document.createElement("div");
             cell.classList.add("cell");
             row.appendChild(cell);
         }
-    
+        
         gridContainer.appendChild(row);
     }
 
@@ -34,7 +64,9 @@ function resetGrid(){
 }
 
 function draw(event){
-    event.target.style.backgroundColor = "black";  
+    if(isClicked){
+        event.target.style.backgroundColor = "black"; 
+    }
 }
 
 createGrid(defaultGridSize);
